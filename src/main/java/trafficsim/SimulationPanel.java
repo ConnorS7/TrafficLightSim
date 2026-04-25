a274ab0011651fa543e7dd34511da3f5dec7f0cd
ConnorS7 <dash4182@colorado.edu>
2026-04-25 16:42:51 -0600

    Added more UI features and pretty sure am done!

--- src/main/java/trafficsim/SimulationPanel.java
+++ src/main/java/trafficsim/SimulationPanel.java
@@ -1,9 +1,10 @@
 package trafficsim;
 
 import trafficsim.model.Direction;
-import trafficsim.model.Intersection;
 import trafficsim.model.TrafficLight;
 import trafficsim.decorator.ICar;
+import trafficsim.strategy.BusyMovementStrategy;
+import trafficsim.strategy.CalmMovementStrategy;
 import trafficsim.strategy.DefaultMovementStrategy;
 import trafficsim.strategy.MovementStrategy;
 
@@ -14,10 +15,10 @@ import java.awt.*;
 public class SimulationPanel extends JPanel {
 
     private final TrafficSimulationFacade simulation;
-    private MovementStrategy movementStrategy = new DefaultMovementStrategy();
     private long lastUpdate;
 
     public SimulationPanel() {
+        MovementStrategy movementStrategy = new DefaultMovementStrategy();
         this.simulation = new TrafficSimulationFacade(movementStrategy);
 
         lastUpdate = System.currentTimeMillis();
@@ -45,18 +46,46 @@ public class SimulationPanel extends JPanel {
     private void setUpControls(){
         JButton pauseBtn = new JButton("Pause");
         JButton resumeBtn = new JButton("Resume");
+
         JSlider speedSlider = new JSlider(1, 5, 1);
 
+        JButton aggressiveBtn = new JButton("Aggressive");
+        JButton safeBtn = new JButton("Safe");
+        JButton normalBtn = new JButton("Normal");
+        JLabel modeLabel = new JLabel("Mode: Normal Traffic");
+
         pauseBtn.setBounds(10, 10, 80, 25);
-        resumeBtn.setBounds(100, 10, 90, 25);
+        resumeBtn.setBounds(100, 10, 80, 25);
 
         speedSlider.setBounds(10, 50, 125, 50);
         speedSlider.setMajorTickSpacing(2);
         speedSlider.setPaintTicks(true);
         speedSlider.setPaintLabels(true);
+        speedSlider.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+        aggressiveBtn.setBounds(390, 10, 100, 25);
+        safeBtn.setBounds(390, 45, 100, 25);
+        normalBtn.setBounds(390, 80, 100, 25);
+        modeLabel.setBounds(350, 115, 150, 25);
+        modeLabel.setForeground(Color.WHITE);
 
         pauseBtn.addActionListener(e -> simulation.pause());
         resumeBtn.addActionListener(e -> simulation.resume());
+
+        aggressiveBtn.addActionListener(e -> {
+                simulation.setStrategy(new BusyMovementStrategy());
+                modeLabel.setText("Mode: Rush Hour Traffic");
+        });
+
+        safeBtn.addActionListener(e -> {
+            simulation.setStrategy(new CalmMovementStrategy());
+            modeLabel.setText("Mode: Calm Traffic");
+        });
+
+        normalBtn.addActionListener(e -> {
+            simulation.setStrategy(new DefaultMovementStrategy());
+            modeLabel.setText("Mode: Normal Traffic");
+        });
 
         speedSlider.addChangeListener(e -> {
             int value = speedSlider.getValue();
@@ -67,6 +96,10 @@ public class SimulationPanel extends JPanel {
         add(pauseBtn);
         add(resumeBtn);
         add(speedSlider);
+        add(aggressiveBtn);
+        add(safeBtn);
+        add(normalBtn);
+        add(modeLabel);
     }
 
     protected void paintComponent(Graphics g){
